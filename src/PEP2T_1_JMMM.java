db4c93c36abb4665d831f6eb8cb5726868814164
Jose Maria <jmateosmartin@safareyese.es>
2024-01-10 23:21:35 +0100

    Primero

---
+++ src/PEP2T_1_JMMM.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PEP2T_1_JMMM {
+    float saldoInicio;
+    float topeRetirar;
+
+    private PEP2T_1_JMMM(float saldazo, float topazo){
+        saldoInicio = saldazo;
+        topeRetirar = topazo;
+    }
+}
