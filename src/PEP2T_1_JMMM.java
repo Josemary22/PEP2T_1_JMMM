854c7fb0775a57b17d8fc59aa067d9b3dc2aa790
jmateosmartin@safareyes.es <jmateosmartin@safareyes.es>
2024-01-11 13:59:24 +0100

    Quinto

--- src/PEP2T_1_JMMM.java
+++ src/PEP2T_1_JMMM.java
@@ -46,4 +46,31 @@ public class PEP2T_1_JMMM {
         saldoInicio += ingresado;
         System.out.println("Su saldo actual es de " + saldoInicio + " €");
     }
+
+    public static void main(String[] args){
+        boolean menu = true;
+        PEP2T_1_JMMM objeto = new PEP2T_1_JMMM(Float.parseFloat(args[0]), Float.parseFloat(args[1]));
+        System.out.println("PROGRAMA CAJERO AUTOMÁTICO");
+        while(menu){
+            System.out.println("\t Menú de Opciones");
+            System.out.println("\t ================");
+            System.out.println("1) Retirada de dinero");
+            System.out.println("2) Ingreso de dinero");
+            System.out.println("3) Salir");
+            System.out.println("Opción: ");
+            Scanner teclado = new Scanner(System.in);
+            int opcion = teclado.nextInt();
+            switch (opcion){
+                case 1:
+                    objeto.Retirar();
+                    break;
+                case 2:
+                    objeto.Ingresar();
+                    break;
+                case 3:
+                    menu = false;
+                    break;
+            }
+        }
+    }
 }
